012cb088bf59fd17243c42ef72ac67c1d936dfcc
sun-san567 <gsasuguru.ome@gmail.com>
2025-03-16 20:06:26 +0900

    自部門のフェーズのみ表示させることに成功

--- app/Http/Controllers/PhaseController.php
+++ app/Http/Controllers/PhaseController.php
@@ -14,7 +14,31 @@ class PhaseController extends Controller
      */
     public function index()
     {
-        $phases = Phase::orderBy('order', 'asc')->get();
+        // 現在のログインユーザーの情報を取得
+        $user = auth()->user();
+        $companyId = $user->company_id;
+        $userDepartmentId = $user->department_id;
+        
+        // 管理者の場合は会社内の全フェーズ、それ以外は自部門のフェーズのみ
+        if ($user->role === 'admin') {
+            // 管理者: 自社の全部門のフェーズを表示
+            $departmentIds = Department::where('company_id', $companyId)
+                ->pluck('id')
+                ->toArray();
+                
+            $phases = Phase::whereIn('department_id', $departmentIds)
+                ->orderBy('order', 'asc')
+                ->get();
+        } else {
+            // 一般ユーザー: 自部門のフェーズのみ表示
+            $phases = Phase::where('department_id', $userDepartmentId)
+                ->orderBy('order', 'asc')
+                ->get();
+        }
+        
+        // 部門名も表示できるようにリレーションを読み込む
+        $phases->load('department');
+        
         return view('phases.index', compact('phases'));
     }
 
--- app/Models/Phase.php
+++ app/Models/Phase.php
@@ -9,10 +9,18 @@ class Phase extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'description', 'order'];
+    protected $fillable = ['name', 'description', 'order', 'department_id'];
 
     public function projects()
     {
         return $this->hasMany(Project::class);
+    }
+
+    /**
+     * このフェーズが所属する部門を取得
+     */
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
     }
 }
