31293043979e9ea0d850070c4cc524d0098819f4
sun-san567 <gsasuguru.ome@gmail.com>
2025-03-22 13:20:19 +0900

    リダイレクト先の変更

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,13 +7,20 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
+    /**
+     * ログイン後のリダイレクト先
+     */
+    public const HOME = '/projects';
+
     public function boot(): void
     {
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api')
                 ->group(base_path('routes/api.php'));
-            // ...
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
         });
     }
 }
